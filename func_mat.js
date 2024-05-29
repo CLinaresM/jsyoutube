e7021c7ebbe246886d90bb23ebdf0640e7d39771
CLinaresM <carlos.linaresm@cun.edu.co>
2024-05-29 15:58:36 -0500

    clase 11-14

---
+++ func_mat.js
@@ -0,0 +1,23 @@
+//redondear a siguiente entero en decimal .5
+var precio = Math.round(399.50);
+document.write('Precio redondeado: ',precio);
+document.write('</br>');
+
+//redondear sin importar decimal hacia arriba
+var precio = Math.ceil(299.3);
+document.write('Precio redondeado: ',precio);
+document.write('</br>');
+
+//redondear sin importar decimal hacia abajo
+var precio = Math.floor(199.2);
+document.write('Precio redondeado: ',precio);
+document.write('</br>');
+
+//calcula el seno de un angulo
+var seno = Math.sin(45);
+document.write('Seno de 45: ', seno);
+document.write('</br>');
+
+
+var expo = Math.exp(2);
+document.write(expo)
