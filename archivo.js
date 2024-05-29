a9f3be0b7c808009376c91f3b55cfe3aaa86d66c
YovannyLinares26 <cargiolimo26@hotmail.com>
2024-05-29 07:51:55 -0500

    Curso JS 1-11

---
+++ archivo.js
@@ -0,0 +1,18 @@
+/*var nombre = "Jose";
+var Nombre = " Paola";
+var NOMBRE  = "MArcela";
+var _nombre = "Mauren";
+var $_nombre = "rebeca";
+
+
+let saludo = "Jose";
+let Saludo = " Paola";
+let SALUDO  = "MArcela";
+let _saludo = "Mauren";
+let $_saludo = "Rebeca";
+
+
+document.write($_saludo);*/
+
+
+
