8af50613e83dbbc2aa7892870a79e94174b06459
CLinaresM <carlos.linaresm@cun.edu.co>
2024-05-30 16:28:05 -0500

    clase 15-16

--- func_mat.js
+++ func_mat.js
@@ -18,6 +18,44 @@ var seno = Math.sin(45);
 document.write('Seno de 45: ', seno);
 document.write('</br>');
 
-
+// calcular el exponencial
 var expo = Math.exp(2);
-document.write(expo)
+document.write('El exponente de 2 es: ',expo);
+document.write('</br>');
+
+//calcular el logaritmo 
+var logaritmo = Math.log(10);
+document.write('Logaritmo de 10: ',logaritmo);
+document.write('</br>');
+
+//calcular valor absoluto
+var absoluto = Math.abs(-10);
+document.write('V abs de -10: ',absoluto);
+document.write('</br>');
+
+//calcular valor maximo de secuencia
+var maximo = Math.max(2, 10, 500, 300, 250,3,8);
+document.write('Mayor valor: ',maximo);
+document.write('</br>');
+
+//calcular valor minimo de secuencia
+var minimo = Math.min(2, 10, 500, 300, 250,3,8);
+document.write('Minimo valor: ',minimo);
+document.write('</br>');
+
+//traer a pantalla valor aleatorio
+
+var aleatorio = Math.round(Math.random()*100);
+document.write('valor aleatorio: ',aleatorio)
+document.write('</br>');
+
+// raiz cuadrada de un numero
+var valor = Math.sqrt(81);
+document.write('raiz cuadrade de 81: ',valor)
+document.write('</br>');
+
+//calcular el exponente de un numero
+
+var  exponente = Math.pow(5, 2);
+document.write('valor de 5 a la 2: ', exponente );
+document.write('</br>');
